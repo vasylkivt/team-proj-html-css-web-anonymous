bf1c76b51dfbec4da3eaad63ec221842b74770d1
Ivano4ka8 <ivannamihalova@gmail.com>
2023-02-28 12:25:25 +0200

    add restaurant-slider

---
+++ src/js/slider-restaurant.js
@@ -0,0 +1,28 @@
+$('.slider-container-main-restaurant').slick({
+  infinite: false, //* безкінечний скрол
+  speed: 300, //* швидкість анімації скролу
+  slidesToScroll: 1, //* кількість карнинок при скролі
+  // slidesToShow: 4,      //* кількість елементів
+  // autoplay: true, //* автоскрол
+  autoplaySpeed: 2000, //* період автоскролу
+  variableWidth: true, //* своя ширина
+
+  prevArrow: $('.slider-btn-prev'),
+  nextArrow: $('.slider-btn-next'),
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        variableWidth: false,
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        variableWidth: false,
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
